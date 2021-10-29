a09cbe1b63aae83d62f3ffe091e48033e8eb25cd
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-10-29 10:47:15 +0530

    w5-textmaker

--- test.js
+++ test.js
@@ -1,45 +1,45 @@
-   // Example of Texpro
+// Example of Texpro
 
 
-const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+   const w5botapi = require('w5-textmaker');
 
-w5botapi.textpro("<textpro_html>",
-    // "Message One" (One Message)
-    // ["Message One"], ["Message Two"] (Two Message)
-    ).then(async (data) => { 
-      try { 
-          console.log(dat)
-      } catch(err) { 
-          console.log(err)
-      } 
-});
+   w5botapi.textpro("<textpro_html>",
+       // "Message One" (One Message)
+       // ["Message One"], ["Message Two"] (Two Message)
+   ).then(async(data) => {
+       try {
+           console.log(dat)
+       } catch (err) {
+           console.log(err)
+       }
+   });
 
    // Example of Photooxy
 
-const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+   const w5botapi = require('w5-textmaker');
 
-w5botapi.photooxy("<photooxy_html>",
-    // "Message One" (One Message)
-    // ["Message One"], ["Message Two"] (Two Message)
-    ).then(async (data) => { 
-      try { 
-          console.log(dat)
-      } catch(err) { 
-          console.log(err)
-      } 
-});
+   w5botapi.photooxy("<photooxy_html>",
+       // "Message One" (One Message)
+       // ["Message One"], ["Message Two"] (Two Message)
+   ).then(async(data) => {
+       try {
+           console.log(dat)
+       } catch (err) {
+           console.log(err)
+       }
+   });
 
    // Example of Ephoto
 
-const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+   const w5botapi = require('w5-textmaker');
 
-w5botapi.ephoto("<ephoto_html>",
-    // "Message One" (One Message)
-    // ["Message One"], ["Message Two"] (Two Message)
-    ).then(async (data) => { 
-      try { 
-          console.log(dat)
-      } catch(err) { 
-          console.log(err)
-      } 
-});
+   w5botapi.ephoto("<ephoto_html>",
+       // "Message One" (One Message)
+       // ["Message One"], ["Message Two"] (Two Message)
+   ).then(async(data) => {
+       try {
+           console.log(dat)
+       } catch (err) {
+           console.log(err)
+       }
+   });
