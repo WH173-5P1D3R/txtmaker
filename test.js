874efcbbb516198c08bdf8501514aaf098ff2033
WH173 5P1D3R <84314826+WH173-5P1D3R@users.noreply.github.com>
2021-10-29 10:35:13 +0530

    w5bot-textmaker

---
+++ test.js
@@ -0,0 +1,45 @@
+   // Example of Texpro
+
+
+const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+
+w5botapi.textpro("<textpro_html>",
+    // "Message One" (One Message)
+    // ["Message One"], ["Message Two"] (Two Message)
+    ).then(async (data) => { 
+      try { 
+          console.log(dat)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+
+   // Example of Photooxy
+
+const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+
+w5botapi.photooxy("<photooxy_html>",
+    // "Message One" (One Message)
+    // ["Message One"], ["Message Two"] (Two Message)
+    ).then(async (data) => { 
+      try { 
+          console.log(dat)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
+
+   // Example of Ephoto
+
+const w5botapi = require('w5bot-textmaker'); // Import NPM Package
+
+w5botapi.ephoto("<ephoto_html>",
+    // "Message One" (One Message)
+    // ["Message One"], ["Message Two"] (Two Message)
+    ).then(async (data) => { 
+      try { 
+          console.log(dat)
+      } catch(err) { 
+          console.log(err)
+      } 
+});
